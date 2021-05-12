91ca00bee977a445c18eeeb62c879a99582e13d2
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2021-05-12 18:52:02 -0500

    Tests for "displayInOrder" method added

--- tests-bst.cpp
+++ tests-bst.cpp
@@ -1,3 +1,7 @@
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <string>
 #include "catch.hpp"
 #include "bst.h"
 
@@ -63,6 +67,106 @@ TEST_CASE("Getting the maximum value in a BST", "[BSTree][maxValue]") {
       items.insert(i);
     
     REQUIRE(items.maxValue() == end - 1);
+  }
+  
+}
+
+// Builds a string with a equal formatting to that from the methods
+// displayInOrder, displayPreOrder, displayPostOrder.
+
+std::string build_string_display(int items[], int size) {
+  std::string result = "";
+  std::for_each(items, items + size, [&](const int& item) {
+    result = result + std::to_string(item) + ", ";
+  });
+  return result;
+}
+
+TEST_CASE("Traversing the tree using the inorder variant", "[BSTree][displayInOrder]") {
+  BSTree<int> items;
+  auto insert = [&] (const int& item) { items.insert(item); };
+  ostringstream oss;
+  streambuf* p_cout_streambuf = cout.rdbuf();
+
+  SECTION("explicitly inserting") {
+
+    SECTION("1 element") {
+      items.insert(10);
+
+      cout.rdbuf(oss.rdbuf());
+      items.displayInOrder();
+      cout.rdbuf(p_cout_streambuf);
+
+      REQUIRE(oss.str() == "10, ");
+    }
+
+    SECTION("2 elements") {
+      items.insert(10);
+      items.insert(6);
+
+      cout.rdbuf(oss.rdbuf());
+      items.displayInOrder();
+      cout.rdbuf(p_cout_streambuf);
+
+      REQUIRE(oss.str() == "6, 10, ");
+    }
+
+    SECTION("3 elements") {
+      int size = 3;
+      int to_insert[size] = {3, 3, 3};
+      int correct_order[size] = {3, 3, 3};
+
+      for_each(to_insert, to_insert + size, insert);
+      
+      cout.rdbuf(oss.rdbuf());
+      items.displayInOrder();
+      cout.rdbuf(p_cout_streambuf);
+
+      REQUIRE(oss.str() == build_string_display(correct_order, size));
+    }
+
+    SECTION("4 elements") {
+      int size = 4;
+      int to_insert[size] = {6, 5, 4, 3};
+      int correct_order[size] = {3, 4, 5, 6};
+
+      for_each(to_insert, to_insert + size, insert);
+      
+      cout.rdbuf(oss.rdbuf());
+      items.displayInOrder();
+      cout.rdbuf(p_cout_streambuf);
+
+      REQUIRE(oss.str() == build_string_display(correct_order, size));
+    }
+
+    SECTION("6 elements") {
+      int size = 6;
+      int to_insert[size] = {10, 6, 4, 3, 5, 8};
+      int correct_order[size] = {3, 4, 5, 6, 8, 10};
+
+      for_each(to_insert, to_insert + size, insert);
+      
+      cout.rdbuf(oss.rdbuf());
+      items.displayInOrder();
+      cout.rdbuf(p_cout_streambuf);
+
+      REQUIRE(oss.str() == build_string_display(correct_order, size));
+    }
+
+    SECTION("9 elements") {
+      int size = 9;
+      int to_insert[size] = {8, 3, 1, 6, 4, 7, 10, 14, 13};
+      int correct_order[size] = {1, 3, 4, 6, 7, 8, 10, 13, 14};
+
+      for_each(to_insert, to_insert + size, insert);
+
+      cout.rdbuf(oss.rdbuf());
+      items.displayInOrder();
+      cout.rdbuf(p_cout_streambuf);
+
+      REQUIRE(oss.str() == build_string_display(correct_order, size));
+    }
+    
   }
   
 }
